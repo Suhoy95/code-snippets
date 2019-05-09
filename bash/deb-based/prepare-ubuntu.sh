08c8b7d46ba19871c5531748162cfd0d1f4a676d
Ilya Sukhoplyuev <suhoy95@gmail.com>
2019-05-09 19:36:10 +0300

    fixes

--- bash/deb-based/prepare-ubuntu.sh
+++ bash/deb-based/prepare-ubuntu.sh
@@ -22,20 +22,19 @@ sudo apt-get install                  \
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
 sudo add-apt-repository 'deb https://typora.io/linux ./'
-sudo apt-get update
 sudo apt-get install typora
 
 sudo mkdir /windows
 sudo fdisk -l
-sudo mount -t ntfs -o nls=utf8,umask=0077,uid=$(id -u suhoy),gid=$(id -g suhoy) \
-    /dev/sdXXX /windows
-sudo blkid
+# sudo mount -t ntfs -o nls=utf8,umask=0077,uid=$(id -u suhoy),gid=$(id -g suhoy) \
+#     /dev/sdXXX /windows
+sudo blkid && id
 cat <<EOF >> /etc/fstab
 # windows partition
-UUID=E8E8F0FBE8F0C938 /windows ntfs nls=utf8,umask=0077,uid=1000,gid=1000 0 0
+UUID=06A0CBF1A0CBE4F1 /windows ntfs nls=utf8,umask=0077,uid=1000,gid=1000 0 0
 EOF
 
-for $dirname in Documents Music Videos Downloads Pictures Desktop; do
+for dirname in Documents Music Videos Downloads Pictures Desktop; do
     tree $dirname
     read -p "mv $dirname Continue?"
     mv $dirname/* /windows/Users/suhoy/$dirname/
