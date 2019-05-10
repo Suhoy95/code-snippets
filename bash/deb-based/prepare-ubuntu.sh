022ec4b6dd358925cee9550ed88a812dbab36d39
Ilya Sukhoplyuev <suhoy95@gmail.com>
2019-05-10 10:59:04 +0300

    Add tmux

--- bash/deb-based/prepare-ubuntu.sh
+++ bash/deb-based/prepare-ubuntu.sh
@@ -14,7 +14,7 @@ sudo apt-get install                  \
     gimp inkscape                     \
     apt-transport-https               \
     fortune-mod                       \
-    mc vim mutt htop \
+    mc vim mutt htop tmux \
     gnome-disk-utility	\
     dstat tree \
     sshfs \
